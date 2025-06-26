fc4062fec99f6d19d4c09b2ede1a7d6373f85deb
PhantomOO7 <md.sahi.ug23@nsut.ac.in>
2025-06-27 01:44:35 +0530

    Switched to file uploader for .pkl files

--- app.py
+++ app.py
@@ -4,48 +4,55 @@ import pickle as pkl
 import pandas as pd
 
 def fetch_poster(movie_id):
-  response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=ed5d3391d93b228448f662150c9022ae&language=en-US'.format(movie_id))
-  data = response.json()
-  return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=ed5d3391d93b228448f662150c9022ae&language=en-US'.format(movie_id)
+    )
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
 def recommend(movie):
-  movie_index= movies[movies['title']==movie].index[0]
-  distance=similarity[movie_index]
-  movies_list=sorted(list(enumerate(distance)),reverse=True,key=lambda x:x[1])[1:6]
-  recommended_movies=[]
-  recommended_movies_poster=[]
-  for i in movies_list:
-    movie_id=movies.iloc[i[0]].movie_id
+    movie_index = movies[movies['title'] == movie].index[0]
+    distance = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    recommended_movies_poster = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
 
-    recommended_movies.append(movies.iloc[i[0]].title)
-    recommended_movies_poster.append(fetch_poster(movie_id))
-  return recommended_movies,recommended_movies_poster
-
-movies = pkl.load(open('movies.pkl', 'rb'))
-movies=pd.DataFrame(movies)
-
-similarity=pkl.load(open('similarity.pkl', 'rb'))
-
+# File upload UI
 st.title('Movie Recommender System')
-selected_movie=st.selectbox('search movies',movies['title'].values)
 
-if st.button('recommend movies'):
-    names,posters=recommend(selected_movie)
+movies_file = st.file_uploader("Upload movies.pkl", type="pkl")
+similarity_file = st.file_uploader("Upload similarity.pkl", type="pkl")
 
-    col1, col2,col3,col4,col5=st.columns(5)
-    with col1:
-      st.text(names[0])
-      st.image(posters[0])
-    with col2:
-      st.text(names[1])
-      st.image(posters[1])
-    with col3:
-      st.text(names[2])
-      st.image(posters[2])
-    with col4:
-      st.text(names[3])
-      st.image(posters[3])
-    with col5:
-      st.text(names[4])
-      st.image(posters[4])
+if movies_file and similarity_file:
+    movies = pd.DataFrame(pkl.load(movies_file))
+    similarity = pkl.load(similarity_file)
+
+    selected_movie = st.selectbox('search movies', movies['title'].values)
+
+    if st.button('recommend movies'):
+        names, posters = recommend(selected_movie)
+
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(names[0])
+            st.image(posters[0])
+        with col2:
+            st.text(names[1])
+            st.image(posters[1])
+        with col3:
+            st.text(names[2])
+            st.image(posters[2])
+        with col4:
+            st.text(names[3])
+            st.image(posters[3])
+        with col5:
+            st.text(names[4])
+            st.image(posters[4])
+else:
+    st.warning("Please upload both `movies.pkl` and `similarity.pkl` files to continue.")
